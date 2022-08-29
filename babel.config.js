57d7e8b0f8efb5e3005452d154deb7a1e90df10c
jhahn678 <jhahn678@yahoo.com>
2022-08-29 15:07:04 -0400

    dotenv and paper plugin

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,11 @@ export default function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    env: {
+      production: {
+        plugins: ['react-native-paper/babel'],
+      },
+    },
     plugins: [
       ["module:react-native-dotenv",{
           moduleName: "@env",
