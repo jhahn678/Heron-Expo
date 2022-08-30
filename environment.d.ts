ea08bb8369f89780cbd97a8b05b4dde09f1dcf69
jhahn678 <jhahn678@yahoo.com>
2022-08-30 16:50:43 -0400

    Added client keys for social login

--- environment.d.ts
+++ environment.d.ts
@@ -1,3 +1,8 @@
 declare module '@env'{
     export const API_BASE_URL: string
+    export const GOOGLE_EXPO_CLIENT_ID: string
+    export const GOOGLE_IOS_CLIENT_ID: string
+    export const GOOGLE_ANDROID_CLIENT_ID: string
+    export const GOOGLE_WEB_CLIENT_ID: string
+    export const FACEBOOK_CLIENT_ID: string
 }
