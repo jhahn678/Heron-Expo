da798fcbc0248370687b6663e9ba657388b08093
jhahn678 <jhahn678@yahoo.com>
2022-09-02 23:35:30 -0400

    Boilerplate and navigation

--- src/screens/explore/SearchResultsScreen/SearchResultsScreen.tsx
+++ src/screens/explore/SearchResultsScreen/SearchResultsScreen.tsx
@@ -1,8 +1,24 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { useLocationStore } from '../../../store/location/useLocationStore'
+import { useSearchParamStore } from '../../../store/search/useSearchParamStore'
 import { ExploreStackScreenProps } from '../../../types/navigation'
 
 const SearchResultsScreen = (props: ExploreStackScreenProps<'SearchResultsScreen'>) => {
+
+  const {
+    hasCoordinates,
+    latitude,
+    longitude
+  } = useLocationStore()
+
+  const {
+    value,
+    classifications,
+    adminOne,
+    geoplace,
+    sort
+  } = useSearchParamStore()
 
   return (
     <View style={styles.container}>
