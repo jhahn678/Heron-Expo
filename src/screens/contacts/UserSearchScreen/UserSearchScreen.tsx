4cd4484e18a4792c6e13c043eb5e02fe8c2d7a9c
jhahn678 <jhahn678@yahoo.com>
2022-09-02 23:33:51 -0400

    Boilerplate for screens

---
+++ src/screens/contacts/UserSearchScreen/UserSearchScreen.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
+import { Text, Title } from 'react-native-paper'
+
+const SearchUsersScreen = () => {
+    
+    return (
+        <View>
+            <Text>SearchUsersScreen</Text>
+        </View>
+    )
+}
+
+export default SearchUsersScreen
+
+const styles = StyleSheet.create({})
---
+++ src/screens/profile/UserProfileScreen/UserProfileScreen.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { StyleSheet, ScrollView } from 'react-native'
+import { Text } from 'react-native-paper'
+
+const UserProfileScreen = () => {
+  return (
+    <ScrollView>
+      <Text>UserProfileScreen</Text>
+    </ScrollView>
+  )
+}
+
+export default UserProfileScreen
+
+const styles = StyleSheet.create({})
