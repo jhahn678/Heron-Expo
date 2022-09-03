541996b9976021d220985d963d65fb3c97cbd516
jhahn678 <jhahn678@yahoo.com>
2022-09-02 23:28:38 -0400

    Enum for keys in secure store

---
+++ src/types/SecureStore.ts
@@ -0,0 +1,9 @@
+export interface ISecureStoreKeys {
+    ACCESS_TOKEN: 'ACCESS_TOKEN'
+    REFRESH_TOKEN: 'REFRESH_TOKEN'
+}
+
+export const SecureStoreKeys: ISecureStoreKeys = {
+    ACCESS_TOKEN: 'ACCESS_TOKEN',
+    REFRESH_TOKEN: 'REFRESH_TOKEN'
+}
