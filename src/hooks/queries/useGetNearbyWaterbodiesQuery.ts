9afb59937d1dd677d1454abc6f9b2537f1f60131
jhahn678 <jhahn678@yahoo.com>
2022-09-04 00:57:00 -0400

    Modified query to allow for more variables

--- src/hooks/queries/useGetNearbyWaterbodiesQuery.ts
+++ src/hooks/queries/useGetNearbyWaterbodiesQuery.ts
@@ -1,37 +1,41 @@
 import { gql, useQuery } from '@apollo/client'
-import { NearbyWaterbody } from '../../types/Waterbody'
+import { NearbyWaterbody, WaterbodyClassification } from '../../types/Waterbody'
 
 const GET_WATERBODIES = gql`
-    query GetWaterbodies($queryLocation: QueryLocation, $limit: Int, $sort: Sort) {
-        waterbodies(queryLocation: $queryLocation, limit: $limit, sort: $sort) {
-            id
-            name
-            classification
-            country
-            ccode
-            subregion
-            admin_one
-            admin_two
-            media {
-                url
-            }
-            distance
-            rank
-            total_catches
-            total_locations
+query Waterbodies($classifications: [ClassificationEnum!], $queryLocation: QueryLocation, $offset: Int, $limit: Int, $sort: Sort) {
+    waterbodies(classifications: $classifications, queryLocation: $queryLocation, offset: $offset, limit: $limit, sort: $sort) {
+        id
+        name
+        classification
+        country
+        ccode
+        subregion
+        admin_one
+        admin_two
+        media {
+            url
         }
+        distance
+        rank
+        total_catches
+        total_locations
     }
-`
+}`
+
+
 
 interface GetNearbyWaterbodiesArgs {
     longitude: number | null,
-    latitude: number | null
+    latitude: number | null,
+    classification?: WaterbodyClassification
+    sort?: 'distance' | 'rank'
+    limit?: number
 }
 
 type GetNearbyWaterbodies= { waterbodies: NearbyWaterbody[] }
 
 export const useGetNearbyWaterbodiesQuery = ({
-    longitude, latitude
+    longitude, latitude, classification, sort='distance', limit=5
 }: GetNearbyWaterbodiesArgs) => {
 
     const result = useQuery<GetNearbyWaterbodies>(GET_WATERBODIES, {
@@ -43,8 +47,9 @@ export const useGetNearbyWaterbodiesQuery = ({
                 latitude, 
                 withinMeters: 100000 
             },
-            limit: 5, 
-            sort: 'distance'
+            classifications: classification ? [classification] : null,
+            limit, 
+            sort
         }
     })
     
