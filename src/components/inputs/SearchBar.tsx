65cd3087b63a6c2693ff553dd89f8a43e4aa937a
jhahn678 <jhahn678@yahoo.com>
2022-09-05 01:39:51 -0400

    styling

--- src/components/inputs/SearchBar.tsx
+++ src/components/inputs/SearchBar.tsx
@@ -1,19 +1,42 @@
-import { StyleSheet } from "react-native";
+import { 
+    StyleSheet, 
+    NativeSyntheticEvent,
+    NativeTouchEvent,
+    ViewStyle,
+    StyleProp,
+    Pressable
+} from "react-native";
 import { Searchbar } from "react-native-paper";
 
 interface Props {
-    value: string,
-    setValue: (value: string) => void
+    value?: string,
+    setValue?: (value: string) => void
+    onPress?: (e: NativeSyntheticEvent<NativeTouchEvent>) => void
+    /** @default false */
+    autofocus?: boolean
+    /** @default true */
+    enabled?: boolean
+    style?: StyleProp<ViewStyle>,
+    placeholder?: string
 }
 
-const SearchBar = (props: Props): JSX.Element => {
+const SearchBar = ({
+    style, value, setValue, onPress, autofocus=false, enabled=true, placeholder
+}: Props): JSX.Element => {
     
     return (
-        <Searchbar style={styles.searchbar}
-            theme={{ roundness:  12 }}
-            value={props.value}
-            onChangeText={props.setValue}
-        />
+        <Pressable onPress={onPress}>
+            <Searchbar 
+                placeholder={placeholder}
+                inputStyle={{ fontSize: 14 }}
+                editable={enabled}
+                autoFocus={autofocus}
+                style={[styles.searchbar, style]} 
+                theme={{ roundness:  12 }}
+                value={value || ''}
+                onChangeText={setValue}
+            />
+        </Pressable>
     )
 }
 
