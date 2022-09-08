71dcac2c05397d8d5eaa430dc0cba695aabe512c
jhahn678 <jhahn678@yahoo.com>
2022-09-07 22:12:32 -0400

    Added new modals to portal

--- src/components/modals/ModalPortal.tsx
+++ src/components/modals/ModalPortal.tsx
@@ -1,18 +1,24 @@
 import { Portal } from "react-native-paper";
 import { useModalStore } from '../../store/modal/useModalStore'
 import AuthModal from "./AuthModal";
+import ConfirmUploadModal from "./ConfirmUploadModal";
 import ReauthenticateModal from "./ReauthenticateModal";
+import UploadPartialSuccessModal from "./UploadPartialSuccessModal";
 
 const ModalPortal = () => {
     
-    const authVisible = useModalStore(state => state.authVisible)
-    const reauthenticate = useModalStore(state => state.reauthenticate)
+    const authVisible = useModalStore(state => state.auth)
+    const reauthVisible = useModalStore(state => state.reauthenticate)
+    const confirmUploadVisible = useModalStore(state => state.confirmUpload)
+    const uploadPartialSuccess = useModalStore(state => state.uploadPartialSuccess)
     const dismiss = useModalStore(state => state.dismiss)
     
     return (
         <Portal>
             <AuthModal visible={authVisible} dismiss={dismiss}/>
-            <ReauthenticateModal visible={reauthenticate} dismiss={dismiss}/>
+            <ReauthenticateModal visible={reauthVisible} dismiss={dismiss}/>
+            <ConfirmUploadModal visible={confirmUploadVisible} dismiss={dismiss}/>
+            <UploadPartialSuccessModal visible={uploadPartialSuccess} dismiss={dismiss}/>
         </Portal>
     );
 };
