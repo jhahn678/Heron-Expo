4b148e5b06c1912ba7a4f93ac9051daad0eeec71
jhahn678 <jhahn678@yahoo.com>
2022-09-07 22:14:33 -0400

    to show on successful image upload

---
+++ src/components/modals/UploadSuccessModal.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { StyleSheet, Text } from "react-native";
+import { Button, Dialog } from "react-native-paper";
+
+interface Props {
+    visible: boolean
+    dismiss: () => void
+}
+
+const UploadSuccessModal = (props: Props) => {
+
+    return (
+        <Dialog 
+            theme={{ roundness: 2 }}
+            visible={props.visible}
+            onDismiss={props.dismiss}
+            style={styles.container}
+        >
+            <Dialog.Title style={styles.title}>
+                Thanks you for contributing! 😄
+            </Dialog.Title>
+            <Dialog.Content>
+                <Text style={styles.text}>
+                    Your images have been successfully saved.
+                    Uploading images is a huge help, not only to us,
+                    but also the rest of the users!
+                </Text>
+            </Dialog.Content>
+            <Dialog.Actions>
+                <Button onPress={props.dismiss}>Dismiss</Button>
+            </Dialog.Actions>
+        </Dialog>
+    );
+};
+
+export default UploadSuccessModal;
+
+const styles = StyleSheet.create({
+    container: {
+        height: 200,
+        width: '90%',
+        alignSelf: 'center',
+        position: 'relative',
+        bottom: '10%'
+    },
+    title: {
+        fontSize: 22,
+        fontWeight: '600'
+    },
+    text: {
+        fontWeight: '500',
+        lineHeight: 20
+    }
+});
