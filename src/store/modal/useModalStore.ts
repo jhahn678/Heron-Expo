6b38afcd0dce36539f22f5325a1c4e000b4b324d
jhahn678 <jhahn678@yahoo.com>
2022-09-07 22:09:25 -0400

    modal store for modal UI

--- src/store/modal/useModalStore.ts
+++ src/store/modal/useModalStore.ts
@@ -1,20 +1,40 @@
 import create from 'zustand'
 
 export interface ModalStore {
-    authVisible: boolean
-    setAuthVisible: () => void
+    auth: boolean
+    setAuth: (value?: boolean) => void
     reauthenticate: boolean
-    setReauthenticate: () => void
+    setReauthenticate: (value?: boolean) => void
+    confirmUpload: boolean
+    setConfirmUpload: (value?: boolean) => void
+    uploadPartialSuccess: boolean,
+    setUploadPartialSuccess: (value?: boolean) => void
+    uploadSuccess: boolean,
+    setUploadSuccess: (value?: boolean) => void
+    error: boolean,
+    setError: (value?: boolean) => void
     dismiss: () => void
 }
 
 export const useModalStore = create<ModalStore>((set) => ({
-    authVisible: false,
-    setAuthVisible: () => set({ authVisible: true }),
+    auth: false,
+    setAuth: (value=true) => set({ auth: value }),
     reauthenticate: false,
-    setReauthenticate: () => set({ reauthenticate: true }),
+    setReauthenticate: (value=true) => set({ reauthenticate: value }),
+    confirmUpload: false,
+    setConfirmUpload: (value=true) => set({ confirmUpload: value }),
+    uploadPartialSuccess: false,
+    setUploadPartialSuccess: (value=true) => set({ uploadPartialSuccess: value }),
+    uploadSuccess: false,
+    setUploadSuccess: (value=true) => set({ uploadSuccess: value }),
+    error: false,
+    setError:  (value=true) => set({ error: value }),
     dismiss: () => set({
-        authVisible: false,
-        reauthenticate: false
+        auth: false,
+        reauthenticate: false,
+        confirmUpload: false,
+        uploadPartialSuccess: false,
+        uploadSuccess: false,
+        error: false
     })
 }))
