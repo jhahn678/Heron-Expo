a9e2f01523116086f8c731ad2551ad4d2df7065b
jhahn678 <jhahn678@yahoo.com>
2022-09-08 17:00:50 -0400

    persist s3 images to db

---
+++ src/hooks/mutations/useAddWaterbodyMedia.ts
@@ -0,0 +1,25 @@
+import { gql, useMutation } from '@apollo/client'
+import { MediaInput, WaterbodyMedia } from '../../types/Media'
+
+const ADD_WATERBODY_MEDIA = gql`
+    mutation Mutation($id: Int!, $media: [MediaInput!]!) {
+        addWaterbodyMedia(id: $id, media: $media) {
+            id
+            url
+        }
+    }
+`
+
+interface Variables {
+    id: number,
+    media: MediaInput[]
+}
+
+interface Response {
+    addWaterbodyMedia: Pick<WaterbodyMedia, 'id' | 'url'>[]
+}
+
+export const useAddWaterbodyMediaMutation = () => {
+    const result = useMutation<Response, Variables>(ADD_WATERBODY_MEDIA)
+    return result
+}
