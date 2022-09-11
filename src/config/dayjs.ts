514a680456ceb7cb42a17ed28f1cf511385bd35d
jhahn678 <jhahn678@yahoo.com>
2022-09-11 18:23:41 -0400

    Added plugin

--- src/config/dayjs.ts
+++ src/config/dayjs.ts
@@ -1,7 +1,10 @@
 import calendar from 'dayjs/plugin/calendar'
+import relativeTime from 'dayjs/plugin/relativeTime'
+
 import dayjs from 'dayjs'
 
 dayjs.extend(calendar)
+dayjs.extend(relativeTime)
 
 export default dayjs;
 
