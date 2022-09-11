74a37ab46f91f3e2baf72042feba6fff33ae64ca
jhahn678 <jhahn678@yahoo.com>
2022-09-11 18:38:57 -0400

    Query for waterbody reviews

---
+++ src/hooks/queries/useGetWaterbodyReviews.ts
@@ -0,0 +1,61 @@
+import { gql, useQuery, useLazyQuery } from '@apollo/client'
+import { IUser } from '../../types/User'
+import { IWaterbodyReview } from '../../types/Waterbody'
+
+const GET_REVIEWS = gql`
+    query Waterbody($id: Int!, $limit: Int, $offset: Int) {
+        waterbody(id: $id) {
+            id
+            reviews(limit: $limit, offset: $offset) {
+                id
+                rating
+                text
+                created_at
+                user {
+                    id
+                    fullname
+                    avatar
+                }
+            }
+        }
+    }
+`
+
+export interface GetWaterbodyReview extends Omit<IWaterbodyReview, 'user' | 'waterbody'>{
+    user: Pick<IUser, 'id' | 'fullname' | 'avatar'>
+}
+
+export interface GetWaterbodyReviews {
+    waterbody: {
+        reviews: GetWaterbodyReview[]
+    }
+}
+
+export enum ReviewSort {
+    CreatedAtNewest = 'CREATED_AT_NEWEST',
+    CreatedAtOldest = 'CREATED_AT_OLDEST',
+    RatingHighest = 'RATING_HIGHEST',
+    RatingLowest = 'RATING_LOWEST'
+}
+
+interface Args {
+    id: number
+    limit?: number
+    offset?: number,
+    sort?: ReviewSort
+}
+
+interface Vars extends Args {
+    limit: number,
+    sort?: ReviewSort
+}
+
+export const useGetWaterbodyReviews = ({ 
+    id, 
+    limit=5, 
+    offset=0, 
+    sort=ReviewSort.CreatedAtNewest
+}: Args) => useQuery<GetWaterbodyReviews, Vars>(GET_REVIEWS, { 
+        variables: { id, limit, offset, sort },
+        skip: id === undefined
+    })
