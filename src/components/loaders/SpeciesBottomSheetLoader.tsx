f9c5cc2ae73536c837f47977c6e7080a47ec3b24
jhahn678 <jhahn678@yahoo.com>
2022-09-13 22:48:48 -0400

    loading skeletons

---
+++ src/components/loaders/SpeciesBottomSheetLoader.tsx
@@ -0,0 +1,21 @@
+import React from "react"
+import ContentLoader, { Rect } from "react-content-loader/native"
+
+const SpeciesBottomSheetLoader = () => (
+    <ContentLoader 
+    speed={1}
+    width={320}
+    height={250}
+    viewBox="0 0 320 250"
+    backgroundColor="#e3e3e3"
+    foregroundColor="#e6e6e6"
+  >
+    <Rect x="0" y="12" rx="12" ry="12" width="220" height="20" /> 
+    <Rect x="0" y="45" rx="12" ry="12" width="200" height="30" /> 
+    <Rect x="0" y="100" rx="12" ry="12" width="320" height="20" /> 
+    <Rect x="0" y="180" rx="12" ry="12" width="320" height="20" /> 
+    <Rect x="0" y="140" rx="12" ry="12" width="320" height="20" />
+  </ContentLoader>
+)
+
+export default SpeciesBottomSheetLoader
---
+++ src/components/loaders/WaterbodyHeaderLoader.tsx
@@ -0,0 +1,23 @@
+import React from "react"
+import ContentLoader, { Rect } from "react-content-loader/native"
+
+const WaterbodyHeaderLoader = () => {
+
+  return (
+    <ContentLoader 
+        speed={2}
+        width={270}
+        height={140}
+        viewBox="0 0 270 140"
+        backgroundColor="#e3e3e3"
+        foregroundColor="#e6e6e6"
+    >
+        <Rect x="0" y="0" rx="12" ry="12" width="260" height="30" /> 
+        <Rect x="0" y="40" rx="12" ry="12" width="270" height="25" /> 
+        <Rect x="0" y="75" rx="12" ry="12" width="200" height="35" /> 
+        <Rect x="0" y="120" rx="12" ry="12" width="220" height="20" />
+    </ContentLoader>
+  )
+}
+
+export default WaterbodyHeaderLoader
