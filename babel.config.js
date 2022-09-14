88de245acdc33d56d44f33127bb46f9ff77c6740
jhahn678 <jhahn678@yahoo.com>
2022-09-13 22:43:36 -0400

    reanimated plugin

--- babel.config.js
+++ babel.config.js
@@ -11,7 +11,8 @@ module.exports =  function(api) {
       ["module:react-native-dotenv",{
           moduleName: "@env",
           path: ".env"
-      }]
+      }],
+      'react-native-reanimated/plugin',
     ]
   };
 };
