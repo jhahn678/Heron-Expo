243d3acaaacdfc8cc1bba9c0dc818f0e05a7e2de
jhahn678 <jhahn678@yahoo.com>
2022-09-16 08:20:08 -0400

    For MapScreen -- fetch waterbody geojson

---
+++ src/hooks/queries/useGetWaterbodyGeometries.ts
@@ -0,0 +1,26 @@
+import { useLazyQuery, gql } from "@apollo/client";
+import { GeoJsonObject, Geometry, GeometryCollection } from "geojson";
+
+const GET_WATERBODY_GEOMETRIES = gql`
+  query Waterbody($id: Int!) {
+    waterbody(id: $id) {
+      id
+      geometries
+    }
+  }
+`;
+
+interface GetWaterbodyGeometriesRes {
+    waterbody: {
+        id: number
+        geometries: GeometryCollection<Geometry>
+    }
+}
+
+interface Vars {
+    id: number | undefined
+}
+
+export const useLazyGetWaterbodyGeometries = (variables: Vars) => {
+    return useLazyQuery<GetWaterbodyGeometriesRes, Vars>(GET_WATERBODY_GEOMETRIES, { variables })
+}
