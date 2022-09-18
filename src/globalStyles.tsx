b1f466b522b96fbf6ee6ea6061ac211bbdb5b050
jhahn678 <jhahn678@yahoo.com>
2022-09-17 20:23:07 -0400

    util classes

--- src/globalStyles.tsx
+++ src/globalStyles.tsx
@@ -3,14 +3,19 @@ import { StyleSheet } from "react-native";
 export const DEFAULT_BACKGROUND = 'rgb(242, 242, 242)';
 
 export default StyleSheet.create({
-    frsb: {
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center'
-    },
-    baseline: {
-        flexDirection: 'row', 
-        alignItems: 'baseline' 
-    }
-})
+  frsb: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  baseline: {
+    flexDirection: "row",
+    alignItems: "baseline",
+  },
+  frac: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+});
