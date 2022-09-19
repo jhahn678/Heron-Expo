aea451d1a0ee1ced1d3ac91d9191e6ac7543348c
jhahn678 <jhahn678@yahoo.com>
2022-09-19 14:26:37 -0400

    Catch like mutation

---
+++ src/hooks/mutations/useFavoriteCatch.ts
@@ -0,0 +1,46 @@
+import { ApolloError, gql, useMutation } from '@apollo/client'
+
+const FAVORITE_CATCH = gql`
+  mutation ToggleSaveLocation($id: Int!) {
+    toggleFavoriteCatch(id: $id)
+  }
+`;
+
+interface Args {
+  onCompleted?: (data: Res) => void;
+  onError?: (err: ApolloError) => void;
+}
+
+interface Vars {
+    id: number
+}
+
+interface Res {
+  toggleFavoriteCatch: boolean;
+}
+
+export const useFavoriteCatch = (args?: Args) => {
+    return useMutation<Res, Vars>(FAVORITE_CATCH, {
+      onCompleted: (data) => {
+        if (args && args.onCompleted) args.onCompleted(data);
+      },
+      onError: (err) => {
+        if (args && args.onError) args.onError(err);
+      },
+      update: (cache, { data }, { variables }) => {
+        if(data && variables?.id){
+            cache.writeFragment({
+                id: `Catch:${variables.id}`,
+                fragment: gql`
+                    fragment UpdateCatch on Catch{
+                        is_favorited
+                    }
+                `,
+                data: {
+                    is_favorited: data.toggleFavoriteCatch
+                }
+            })
+        }
+      }
+    });
+}
