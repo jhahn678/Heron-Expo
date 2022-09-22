02ec4dbddeb0c425a67a098c731e356d4bfd167d
jhahn678 <jhahn678@yahoo.com>
2022-09-22 12:20:35 -0400

    Integrating nearby catches to location screen

--- src/screens/location/LocationScreen/sections/CatchesNearby.tsx
+++ src/screens/location/LocationScreen/sections/CatchesNearby.tsx
@@ -2,30 +2,58 @@ import { FlashList } from "@shopify/flash-list";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { GetLocationRes } from "../../../../hooks/queries/useGetLocation";
-import CatchListItem from '../../../../components/lists/CatchesListHorizontal/CatchesListItem'
 import { useGetCatches } from "../../../../hooks/queries/useGetCatches";
 import { CatchQuery, CatchSort } from "../../../../types/Catch";
 import { geomToCoordinates } from "../../../../utils/conversions/geomToCoordinates";
+import RecentActivityCatch from "../../../../components/lists/RecentActivityHorizontal/RecentActivityCatch";
+import { RootStackScreenProps } from "../../../../types/navigation";
+import BoxLoader from "../../../../components/loaders/BoxLoader";
+import CatchesListEmpty from "../../../../components/lists/shared/CatchesListEmpty";
 
 interface Props {
+    navigation: RootStackScreenProps<'ViewLocationScreen'>['navigation']
     geom: GetLocationRes['location']['geom'] | undefined
+    waterbody: GetLocationRes['location']['waterbody']['id'] | undefined
 }
 
-const CatchesNearby = ({ geom }: Props) => {
+const CatchesNearby = ({ waterbody, geom, navigation }: Props) => {
 
-    console.log(geomToCoordinates(geom))
+    const navigateToCatch = (id: number) => () => navigation.navigate('ViewCatchScreen', { id })
 
-    const { data, loading } = useGetCatches({ 
-        type: CatchQuery.Coordinates, 
+    const navigateToUser = (id: number) => () => navigation.navigate('UserProfileScreen', { id })
+
+    const { data, loading, error } = useGetCatches({ 
+        type: CatchQuery.Waterbody,
+        id: waterbody,
         coordinates: geomToCoordinates(geom),
+        within: 50000,
         sort: CatchSort.Nearest,
         limit: 5,
     })
 
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Catches Near Here</Text>
-            {/* <FlashList data={} renderItem={}/> */}
+            <Text style={styles.title}>Catches Nearby</Text>
+            { data ?
+                data?.catches.length > 0 ?
+                <FlashList
+                    horizontal={true}
+                    showsHorizontalScrollIndicator={false}
+                    estimatedItemSize={350}
+                    contentContainerStyle={{ paddingLeft: 16 }}
+                    data={data?.catches} 
+                    renderItem={({ item }) => (
+                        <RecentActivityCatch 
+                            data={item} key={item.id}
+                            onNavigateToCatch={navigateToCatch(item.id)}
+                            onNavigateToProfile={navigateToUser(item.user.id)}
+                        />
+                    )}
+                />
+                : <CatchesListEmpty scale={.7} fontSize={16} caption={'No Catches Nearby'}/>
+                : loading ? new Array(3).fill(null).map((_,i) => <BoxLoader key={i}/>)
+                : null
+            }
         </View>
     );
 };
@@ -34,9 +62,15 @@ export default CatchesNearby;
 
 const styles = StyleSheet.create({
     container: {
-        paddingVertical: 24
+        paddingTop: 24,
+        paddingBottom: 32,
+        borderTopWidth: 1,
+        borderTopColor: '#e0e0e0',
     },
     title: {
-        marginBottom: 24
+        marginBottom: 24,
+        paddingHorizontal: 16,
+        fontSize: 20,
+        fontWeight: '500'
     }
 });
