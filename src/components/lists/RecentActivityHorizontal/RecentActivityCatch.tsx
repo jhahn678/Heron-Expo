04f5e05b9564373c5a6bfacd68b9b37da0dfa3df
jhahn678 <jhahn678@yahoo.com>
2022-09-22 12:28:42 -0400

    Changed layout

--- src/components/lists/RecentActivityHorizontal/RecentActivityCatch.tsx
+++ src/components/lists/RecentActivityHorizontal/RecentActivityCatch.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { 
     StyleSheet, 
-    Pressable, 
     View, 
     StyleProp, 
     ViewStyle, 
-    Image 
+    Image, 
+    Pressable
 } from 'react-native'
-import { Title, Text, useTheme } from 'react-native-paper'
+import { Text } from 'react-native-paper'
 import { RecentActivity } from '../../../hooks/queries/useGetRecentActivity'
 import Avatar from '../../users/Avatar'
 import { dateToCalendar } from '../../../utils/conversions/dateToCalendar'
@@ -23,22 +23,21 @@ const RecentActivityCatch = <T extends RecentActivity>({
     data, style, onNavigateToCatch, onNavigateToProfile
 }: Props<T>): JSX.Element => {
 
-    const { colors } = useTheme()
-
     return (
-        <Pressable onPress={onNavigateToCatch}  style={[styles.container, style, { shadowColor: colors.backdrop}]}>
+        <Pressable onPress={onNavigateToCatch} style={[styles.container, style]}>
             <View style={styles.header}>
                 <Avatar 
-                    size={40}
+                    size={36}
                     uri={data.user.avatar} 
                     onPress={onNavigateToProfile} 
                     fullname={data.user.fullname}
                 />  
-                <View style={styles.headerText}>
+                <View style={styles.user}>
                     <Text style={styles.name}>{data.user.fullname}</Text>    
-                    <Text>{dateToCalendar(data.created_at)}</Text>
+                    <Text style={styles.created}>{dateToCalendar(data.created_at)}</Text>
                 </View>
             </View>
+            <Image source={{ uri: data.media[0]?.url }} style={styles.image}/>
             {
                 data.species ? 
                     <Text style={styles.caption}>
@@ -49,7 +48,6 @@ const RecentActivityCatch = <T extends RecentActivity>({
                         Logged a catch at {data.waterbody.name}
                     </Text>
             }
-            <Image source={{ uri: data.media[0]?.url }} style={styles.image}/>
         </Pressable>
     )
 }
@@ -58,36 +56,36 @@ export default RecentActivityCatch
 
 const styles = StyleSheet.create({
     container: {
-        height: '100%',
+        height: 350,
         width: 300,
+        backgroundColor: 'white',
         marginRight: 32,
-        elevation: 3, 
-        shadowOffset: { 
-            height: 2, 
-            width: 0 
-        }, 
-        shadowRadius: 2, 
-        shadowOpacity: .2,
+        borderRadius: 12,
         justifyContent: 'space-evenly'
     },
     image: {
-        height: '70%',
+        flex: 1,
         width: '100%',
-        borderRadius: 12,
-        
+        backgroundColor: '#e0e0e0'
     },
     header: {
         flexDirection: 'row',
         alignItems: 'center',
+        padding: 12
     },
-    headerText: {
-        paddingLeft: 12
+    user: {
+        paddingLeft: 8
     },
     name: {
-        fontWeight: '600',
-        fontSize: 16
+        fontWeight: '500',
+        fontSize: 14
+    },
+    created: {
+        fontSize: 12,
     },
     caption: {
-        fontWeight: '500'
+        fontSize: 14,
+        fontWeight: '500',
+        padding: 12,
     }
 })
