a1b3c48f10d83ac09d6918b206c43b4182951b04
jhahn678 <jhahn678@yahoo.com>
2022-09-26 09:07:17 -0400

    Added action button for new catch

--- src/navigation/MyCatchesTabs.tsx
+++ src/navigation/MyCatchesTabs.tsx
@@ -1,6 +1,6 @@
 import { View, StyleSheet } from 'react-native'
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
-import { MyCatchesTabsParams } from "../types/navigation";
+import { BottomTabsScreenProps, ExploreStackScreenProps, MyCatchesTabsParams } from "../types/navigation";
 import CatchesTabView from "../screens/catch/MyCatches/CatchesTabView";
 import StatisticsTabView from "../screens/catch/MyCatches/StatisticsTabView";
 import HeadingSection from '../screens/catch/MyCatches/sections/HeadingSection';
@@ -9,10 +9,16 @@ import FilterSpeciesBottomSheet from '../components/modals/myCatches/FilterSpeci
 import FilterWaterbodyBottomSheet from '../components/modals/myCatches/FilterWaterbodyBottomSheet';
 import FilterLengthBottomSheet from '../components/modals/myCatches/FilterLengthBottomSheet';
 import FilterWeightBottomSheet from '../components/modals/myCatches/FilterWeightBottomSheet';
+import WaterbodyTotalsBottomSheet from '../components/modals/myCatches/WaterbodyTotalsBottomSheet';
+import SpeciesTotalsBottomSheet from '../components/modals/myCatches/SpeciesTotalsBottomSheet';
+import { FAB } from 'react-native-paper';
+import FishIcon from '../components/icons/FishIcon';
 
 const Tab = createMaterialTopTabNavigator<MyCatchesTabsParams>();
 
-const MyCatchesTabs = () => {
+const MyCatchesTabs = ({ navigation }: BottomTabsScreenProps<'MyCatchesScreen'>) => {
+
+  const handleNewCatch = () => navigation.navigate('NewCatchScreen')
 
   return (
     <View style={styles.container}>
@@ -29,11 +35,23 @@ const MyCatchesTabs = () => {
           options={{ tabBarLabel: 'Statistics' }}
         />
       </Tab.Navigator>
+      <FAB 
+        icon={({ size, color }) => (
+          <FishIcon color={color} size={size}/>
+        )} 
+        animated={false}
+        onPress={handleNewCatch}
+        customSize={64} 
+        theme={{ roundness: 2 }}
+        style={styles.fab}
+      />
       <FilterDateBottomSheet/>
       <FilterSpeciesBottomSheet/>
       <FilterWaterbodyBottomSheet/>
       <FilterLengthBottomSheet/>
       <FilterWeightBottomSheet/>
+      <WaterbodyTotalsBottomSheet/>
+      <SpeciesTotalsBottomSheet/>
     </View>
   );
 };
@@ -44,6 +62,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     width: '100%'
+  },
+  fab: { 
+    position: 'absolute', 
+    zIndex: 100, 
+    right: 24, 
+    bottom: 36, 
   }
 })
 
