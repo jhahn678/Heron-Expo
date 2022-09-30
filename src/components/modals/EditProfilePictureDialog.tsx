fc26396e67437eb45b790e219d8779ac50d71390
jhahn678 <jhahn678@yahoo.com>
2022-09-30 11:27:43 -0400

    Edit profile picture prompt

---
+++ src/components/modals/EditProfilePictureDialog.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import { Dialog, Button, Text } from "react-native-paper";
+import { useImagePicker } from "../../hooks/utils/useImagePicker";
+import { useImageStore } from "../../store/image/useImageStore";
+
+interface Props {
+    visible: boolean
+    setVisible: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const EditProfilePictureDialog = ({ visible, setVisible }: Props) => {
+
+    const { openImagePickerAvatar, openCamera } = useImagePicker()
+    const setImage = useImageStore(store => store.setImages)
+
+    const handleCamera = async () => {
+        const image = await openCamera()
+        if(image) setImage(image)
+        setVisible(false);
+    }
+
+    const handleGallery = async () => {
+        const image = await openImagePickerAvatar()
+        if(image) setImage(image)
+        setVisible(false);
+    }
+
+    return (
+        <Dialog visible={visible} onDismiss={() => setVisible(false)}>
+            <Dialog.Title>Choose Profile Picture</Dialog.Title>
+            <Dialog.Content>
+                <Text>Take a picture or choose one from your photo library.</Text>
+            </Dialog.Content>
+            <Dialog.Actions>
+                <Button onPress={handleCamera}>Take Picture</Button>
+                <Button onPress={handleGallery}>Open Gallery</Button>
+            </Dialog.Actions>
+        </Dialog>
+    );
+};
+
+export default EditProfilePictureDialog;
+
+const styles = StyleSheet.create({});
