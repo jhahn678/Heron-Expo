f3a0d671bd7ad76a848c4424a1fcd8d233c44809
jhahn678 <jhahn678@yahoo.com>
2022-10-02 00:38:38 -0400

    Styling and follow/unfollow

--- src/components/lists/ContactsList/ContactsListItem.tsx
+++ src/components/lists/ContactsList/ContactsListItem.tsx
@@ -3,20 +3,29 @@ import { Pressable, StyleSheet, Text, View } from "react-native";
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 import { theme } from "../../../config/theme";
 import globalStyles from "../../../globalStyles";
-import { GetMyContacts } from "../../../hooks/queries/useGetMyContacts";
+import { GetMyFollowing } from "../../../hooks/queries/useGetUserFollowing";
 import { useModalStore } from "../../../store/modal/useModalStore";
+import { useFollowUser } from "../../../hooks/mutations/useFollowUser";
 import Avatar from '../../users/Avatar'
+import { TouchableRipple } from "react-native-paper";
 
 interface Props {
-    data: GetMyContacts['me']['contacts'][number]
+    data: GetMyFollowing['me']['following'][number]
     navigateUser: () => void
 }
 
 const ContactsListItem = ({ data, navigateUser }: Props) => {
 
     const setManageContact = useModalStore(store => store.setManageContact)
+    const [followUser] = useFollowUser()
 
-    const handleFriends = () => setManageContact({ name: data.fullname, user: data.id })
+    const handleFollow = () => followUser({ variables: { id: data.id } })
+
+    const handleUnfollow = () => setManageContact({ 
+        user: data.id,
+        name: data.fullname, 
+        username: data.username
+    })
 
     return (
         <View style={styles.container}>
@@ -24,13 +33,15 @@ const ContactsListItem = ({ data, navigateUser }: Props) => {
                 <Avatar fullname={data.fullname} uri={data.avatar} size={56} onPress={navigateUser}/>
                 <View style={styles.text}>
                     <Text style={styles.name}>{data.fullname}</Text>
-                    <Text>{data.location || 'Harrisburg, PA'}</Text>
+                    <Text>{`@${data.username}`}</Text>
                 </View>
             </Pressable>
-            <Pressable style={styles.button} onPress={handleFriends}>
-                <Text style={styles.status}>Friends</Text>
-                <Icon name='check' size={14} color={theme.colors.primary}/>
-            </Pressable>
+            <TouchableRipple style={styles.button} onPress={data.am_following ? handleUnfollow : handleFollow}>
+                <View style={globalStyles.frac}>
+                    <Text style={styles.status}>{data.am_following ? "Following" : "Follow"}</Text>
+                    <Icon name={data.am_following ? 'check' : 'plus'} size={14} color={theme.colors.primary}/>
+                </View>
+            </TouchableRipple>
         </View>
     );
 };
@@ -56,8 +67,6 @@ const styles = StyleSheet.create({
         marginLeft: 12
     },
     button: {
-        flexDirection: 'row',
-        alignItems: 'center',
         paddingHorizontal: 12,
         paddingVertical: 6,
         borderRadius: 6,
