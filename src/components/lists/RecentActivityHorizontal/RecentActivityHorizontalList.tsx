1332d6e24df883b191075a53292a33335d813289
jhahn678 <jhahn678@yahoo.com>
2022-10-02 11:51:01 -0400

    bug fix

--- src/components/lists/RecentActivityHorizontal/RecentActivityHorizontalList.tsx
+++ src/components/lists/RecentActivityHorizontal/RecentActivityHorizontalList.tsx
@@ -2,21 +2,35 @@ import React from 'react'
 import { FlashList } from '@shopify/flash-list'
 import { RecentActivity } from '../../../hooks/queries/useGetRecentActivity'
 import RecentActivityCatch from './RecentActivityCatch'
+import ListFooterSeeMore from '../shared/ListFooterSeeMore'
+import { Button } from 'react-native-paper'
 
 interface Props<T> {
     data: T[],
     onNavigateToCatch: (catchId: number) => void
     onNavigateToProfile: (userId: number) => void
+    onNavigateSeeMore: () => void
 }
 
 const RecentActivityHorizontalList = <T extends RecentActivity>({
-    data, onNavigateToCatch, onNavigateToProfile
+    data, 
+    onNavigateSeeMore,
+    onNavigateToCatch, 
+    onNavigateToProfile,
 }: Props<T>) => {
 
     return (
         <FlashList horizontal data={data} 
             showsHorizontalScrollIndicator={false}
             contentContainerStyle={{ paddingLeft: 20, paddingRight: 24 }}
+            ListFooterComponent={() => (
+                <Button 
+                    icon='arrow-right'
+                    onPress={onNavigateSeeMore}
+                    style={{ marginTop: 180, marginRight: 24 }}
+                    contentStyle={{ flexDirection: 'row-reverse'}}
+                >See More</Button>
+            )}
             estimatedItemSize={300}
             renderItem={({ item }) => (
                 <RecentActivityCatch
