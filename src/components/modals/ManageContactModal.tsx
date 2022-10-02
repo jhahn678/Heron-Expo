5b71a0dba3c16fbf475f05ddeeb021b46a90f5de
jhahn678 <jhahn678@yahoo.com>
2022-10-02 00:39:05 -0400

    Confirm unfollow modal

--- src/components/modals/ManageContactModal.tsx
+++ src/components/modals/ManageContactModal.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Text } from "react-native";
 import { Button, Dialog } from "react-native-paper";
 import { useModalStore } from "../../store/modal/useModalStore";
-import { useRemoveContact } from "../../hooks/mutations/useRemoveContact";
+import { useUnfollowUser } from "../../hooks/mutations/useFollowUser";
 
 interface Props {
     visible: boolean
@@ -11,28 +11,32 @@ interface Props {
 
 const ManageContactModal = ({ visible, dismiss }: Props) => {
 
-    const [removeContact] = useRemoveContact()
+    const [unfollow] = useUnfollowUser()
 
     const store = useModalStore(store => ({ 
+        id: store.manageContactUser,
         name: store.manageContactName, 
-        id: store.manageContactUser
+        username: store.manageContactUsername,
     }))
 
     const handleConfirm = async () => {
         if(!store.id) return;
-        await removeContact({ variables: { id: store.id }})
+        await unfollow({ variables: { id: store.id }})
         dismiss()
     }
 
     return (
         <Dialog visible={visible} onDismiss={dismiss} theme={{ roundness: 1 }}>
-            <Dialog.Title>Manage Friend</Dialog.Title>
+            <Dialog.Title>Manage User</Dialog.Title>
             <Dialog.Content>
-                <Text>Are you sure you want to remove {store.name}?</Text>
+                <Text>
+                    Are you sure you want to unfollow
+                    {store.username ? ` @${store.username}` : ` ${store.name}`}?
+                </Text>
             </Dialog.Content>
             <Dialog.Actions>
                 <Button onPress={dismiss}>Cancel</Button>
-                <Button onPress={handleConfirm}>Remove</Button>
+                <Button onPress={handleConfirm}>Unfollow</Button>
             </Dialog.Actions>
         </Dialog>
     );
