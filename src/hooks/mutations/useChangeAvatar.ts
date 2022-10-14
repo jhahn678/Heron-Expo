26cfdc948eb337d63e36621f20295ba5eabbdbfc
jhahn678 <jhahn678@yahoo.com>
2022-10-13 23:19:30 -0400

    Create account

--- src/hooks/mutations/useChangeAvatar.ts
+++ src/hooks/mutations/useChangeAvatar.ts
@@ -1,5 +1,6 @@
 import { gql, useMutation } from '@apollo/client'
 import { useAuth } from '../../store/auth/useAuth'
+import { GET_MY_PROFILE } from '../queries/useGetMyProfile'
 
 const CHANGE_AVATAR = gql`
 mutation Mutation($avatar: MediaInput!) {
@@ -21,6 +22,7 @@ interface Vars {
 export const useChangeAvatar = () => {
     const { setDetails } = useAuth()
     return useMutation<UseChangeAvatarRes, Vars>(CHANGE_AVATAR, {
+        refetchQueries: [{ query: GET_MY_PROFILE }],
         onCompleted: ({ updateUserAvatar }) => setDetails({ 
             avatar: updateUserAvatar || undefined
         })
--- src/hooks/mutations/useCreateAccount.ts
+++ src/hooks/mutations/useCreateAccount.ts
@@ -1,42 +1,37 @@
 import { useState } from 'react'
 import { axios } from '../../config/axios'
-import { AuthResponse } from '../../store/auth/useAuth'
-
-interface useCreateAccountArgs {
-    onSuccess: (res: AuthResponse) => void,
-    onError: (err: unknown) => void
-}
+import { AuthResponse, useAuth } from '../../store/auth/useAuth'
 
 interface CreateAccountParams {
-    firstName: string,
-    lastName: string,
+    firstname: string,
+    lastname: string,
     email: string,
     username: string,
-    password: string
+    password: string,
+    city: string | undefined
+    state: string | undefined
+    bio: string | undefined
 }
 
-export const useCreateAccount = ({ onSuccess, onError}: useCreateAccountArgs) => {
+export const useCreateAccount = () => {
+
+    const setUser = useAuth(store => store.setUser)
 
     const [isLoading, setIsLoading] = useState(false)
     const [isError, setIsError] = useState(false)
 
     const createAccount = async (params: CreateAccountParams): Promise<AuthResponse | void> => {
         setIsLoading(true)
-        axios.post<AuthResponse>('/auth/register', {
-            firstname: params.firstName,
-            lastname: params.lastName,
-            username: params.username,
-            email: params.email,
-            password: params.password
-        }).then(({ data }) => {
-            setIsLoading(false)
-            onSuccess(data)
+        try{
+            const { data } = await axios.post<AuthResponse>('/auth/register', params)
+            setUser(data); 
+            setIsLoading(false); 
             return data;
-        }).catch(err => {
+        }catch(err){
+            console.log(err)
             setIsLoading(false)
             setIsError(true)
-            onError(err)
-        })
+        }
     }
 
     return {
