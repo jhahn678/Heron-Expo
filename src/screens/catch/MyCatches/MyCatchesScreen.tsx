049b7eef9e3521eea7a248f59dd0020f1df4f604
jhahn678 <jhahn678@yahoo.com>
2022-10-24 13:20:34 -0400

    Sytling touchups

---
+++ src/screens/catch/MyCatches/MyCatchesScreen.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { BottomTabsScreenProps } from '../../types/navigation'
+
+const MyCatchesScreen = (props: BottomTabsScreenProps<'MyCatchesScreen'>): JSX.Element => {
+
+  return (
+    <View>
+      <Text>CatchesListScreen</Text>
+    </View>
+  )
+}
+
+export default MyCatchesScreen;
+
+const styles = StyleSheet.create({})
