b6ebdde5562f2c3c4a4be69d56267d8752d220c7
jhahn678 <jhahn678@yahoo.com>
2022-12-05 08:22:27 -0500

    util function for handling default image

---
+++ src/utils/imageUriHandler.ts
@@ -0,0 +1,16 @@
+import { Image } from 'react-native'
+const DEFAULT_IMAGE = Image.resolveAssetSource(require('../../assets/default-background.png')).uri
+
+interface WaterbodyData{
+    media?: {
+        url: string
+    }[]
+}
+
+export const imageUriHandler = <TData extends WaterbodyData>(data: TData): string => {
+    if(data && data.media && data.media.length){
+        return data.media[0].url;
+    }else{
+        return DEFAULT_IMAGE;
+    }
+}
