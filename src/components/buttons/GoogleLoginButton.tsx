f880aca5359a2e9f3518c080e629987c6a6abf58
jhahn678 <jhahn678@yahoo.com>
2022-12-05 11:01:48 -0500

    issues with env variables

--- src/components/buttons/GoogleLoginButton.tsx
+++ src/components/buttons/GoogleLoginButton.tsx
@@ -6,7 +6,8 @@ import * as Google from 'expo-auth-session/providers/google'
 import { 
   GOOGLE_ANDROID_CLIENT_ID, 
   GOOGLE_EXPO_CLIENT_ID, 
-  GOOGLE_IOS_CLIENT_ID
+  GOOGLE_IOS_CLIENT_ID, 
+  GOOGLE_WEB_CLIENT_ID
 } from '@env'
 import { ResponseType } from 'expo-auth-session'
 import { useGoogleLogin } from '../../hooks/mutations/useGoogleLogin'
@@ -28,12 +29,12 @@ const GoogleLoginButton = ({ navigation }: Props) => {
     expoClientId: GOOGLE_EXPO_CLIENT_ID,
     iosClientId: GOOGLE_IOS_CLIENT_ID,
     androidClientId: GOOGLE_ANDROID_CLIENT_ID,
+    webClientId: GOOGLE_WEB_CLIENT_ID,
     responseType: ResponseType.Token
   });
 
   useEffect(() => {
     if (response?.type === 'success') {
-      console.log(response)
       const { authentication } = response;
       if(!authentication) return;
       (async() => {
